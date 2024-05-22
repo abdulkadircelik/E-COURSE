bb38db1fdcff56402188fa12579cadc8a4da2869
abdulkadircelik <abdulkadircelik267@gmail.com>
2024-05-22 20:44:37 +0300

    First Publish

---
+++ lib/configs/api_path.dart
@@ -0,0 +1,33 @@
+class ApiPath {
+  ApiPath._();
+
+  /*------------------------------- FireStore --------------------------------*/
+  // ignore: constant_identifier_names
+  static const String USER = 'user';
+  // ignore: constant_identifier_names
+  static const String QUIZ = 'quiz';
+
+  // ignore: constant_identifier_names
+  static const String COURSE = 'course';
+  // ignore: constant_identifier_names
+  static const String COURSE_LESSON = 'course_lesson';
+  // ignore: constant_identifier_names
+  static const String COURSE_VIDEO = 'course_video';
+
+  // ignore: constant_identifier_names
+  static const String COMMENT = 'comment';
+
+  // ignore: constant_identifier_names
+  static const String QUESTION = 'question';
+  // ignore: constant_identifier_names
+  static const String LESSON = 'lesson';
+
+  // ignore: constant_identifier_names
+  static const String CONTACT = 'contact';
+
+  /*-------------------------------- Storage ---------------------------------*/
+  // ignore: non_constant_identifier_names
+  static String USER_IMAGES = 'user_images';
+  // ignore: non_constant_identifier_names
+  static String COURSE_AVATAR = 'course_avatar';
+}
---
+++ lib/const/const.dart
@@ -0,0 +1,7 @@
+// ignore: constant_identifier_names
+const DELAY_SPLASH_SCREEN = 1;
+// ignore: constant_identifier_names
+const DEFAULT_AVATAR =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png';
+// ignore: constant_identifier_names
+const VERSION_APP = "1.0";
